a96c601199bbc58b098495bc923904ceaf8d7252
AlvesKZ <igoralvex01@gmail.com>
2026-02-05 15:21:21 -0300

    feat: create DAO factory and Seller DAO implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import entities.Department;
 import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 import java.util.Date;
 
@@ -8,6 +10,9 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "Bob@email.com", new Date(), 3000D, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerSao();
+
         System.out.println(obj);
         System.out.println(seller);
 
