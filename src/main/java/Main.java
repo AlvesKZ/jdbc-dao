4f35294c83bdb80ba07365659f436737daedde43
AlvesKZ <igoralvex01@gmail.com>
2026-02-05 16:23:37 -0300

    feat: create method to instantiate Seller and Department objects

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "Bob@email.com", new Date(), 3000D, obj);
 
+        System.out.println("=== TEST 1: seller findById ===");
         SellerDao sellerDao = DaoFactory.createSellerSao();
 
         Seller seller1 = sellerDao.findById(3);
