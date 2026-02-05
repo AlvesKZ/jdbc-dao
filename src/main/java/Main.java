4db3b71f8dfbc21176a9cd2dc992086d26ef7ce5
AlvesKZ <igoralvex01@gmail.com>
2026-02-05 17:33:12 -0300

    feat: create method to delete a Seller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,12 @@ import model.dao.SellerDao;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerSao();
 
@@ -38,6 +41,12 @@ public class Main {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
 
     }
 }
